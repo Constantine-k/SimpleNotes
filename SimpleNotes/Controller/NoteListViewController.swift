87ae8751a2079974a7235556ed17b3eba2d6fe86
Konstantin Konstantinov <konstantin.k.bm@gmail.com>
2017-12-05 13:52:17 +0200

    Add NoteListViewController

---
+++ SimpleNotes/Controller/NoteListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by Konstantin Konstantinov on 12/4/17.
+//  Copyright © 2017 Konstantin Konstantinov. All rights reserved.
+//
+
+import UIKit
+
+class NoteListViewController: UIViewController {
+
+    @IBOutlet weak var noteTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
